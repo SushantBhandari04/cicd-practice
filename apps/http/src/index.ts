bc8242010a30d45810d9286dcb6739369479f31b
SushantBhandari <sushbh2004@gmail.com>
2025-06-05 11:03:30 +0530

    Created the app

---
+++ apps/http/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express"
+import { client } from "@repo/db/client"
+
+const app = express();
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+})
+
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await client.user.create({
+        data: {
+            username,
+            password
+        }
+    })
+
+    res.status(201).json({
+        message: "User created successfully",
+        id: user.id,
+    });
+})
+
+app.listen(3002, () => {
+    console.log("Server is running on port 3002");
+});
---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import { client } from "@repo/db/client";
+
+export default async function Home() {
+  const user = await client.user.findFirst();
+  return <div>
+    <div>{user?.username}</div>
+    <div>{user?.password}</div>
+  </div>
+}
